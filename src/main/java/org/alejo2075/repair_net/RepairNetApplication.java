4eeb1c01679c95e5f93b68f257d412dba549b782
Alejo2075 <alejsant75@gmail.com>
2024-04-29 23:44:03 -0500

    Initial commit

---
+++ src/main/java/org/alejo2075/repair_net/RepairNetApplication.java
@@ -0,0 +1,13 @@
+package org.alejo2075.repair_net;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepairNetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepairNetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/alejo2075/repair_net/RepairNetApplicationTests.java
@@ -0,0 +1,13 @@
+package org.alejo2075.repair_net;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepairNetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
