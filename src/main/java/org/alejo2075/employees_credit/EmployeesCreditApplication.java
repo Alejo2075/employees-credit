3db146224fc1a6be5ac74b25ca00b9f9910e3994
Alejo2075 <alejsant75@gmail.com>
2024-05-08 23:00:25 -0500

    renamed

--- src/main/java/org/alejo2075/employees_credit/EmployeesCreditApplication.java
+++ src/main/java/org/alejo2075/employees_credit/EmployeesCreditApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GovEmployeesCreditApplication {
+public class EmployeesCreditApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(GovEmployeesCreditApplication.class, args);
+        SpringApplication.run(EmployeesCreditApplication.class, args);
     }
 
 }
