e189a2cc182a17de6dbe5c8e938710d292139c58
Alejo2075 <alejsant75@gmail.com>
2024-05-12 23:30:53 -0500

    updated api doc

--- src/main/java/org/alejo2075/employees_credit/config/SwaggerConfig.java
+++ src/main/java/org/alejo2075/employees_credit/config/SwaggerConfig.java
@@ -1,5 +1,9 @@
 package org.alejo2075.employees_credit.config;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import org.springdoc.core.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,5 +18,16 @@ public class SwaggerConfig {
                 .packagesToScan("org.alejo2075.gov_employees_credit.controller")
                 .build();
     }
-}
 
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Employees Credit API")
+                        .version("1.0")
+                        .description("API documentation for the Employees Credit application")
+                        //.termsOfService("http://example.com/terms")
+                        .contact(new Contact().name("Support Team").email("alejsant75@gmail.com").url("https://github.com/Alejo2075/employees-credit"))
+                        .license(new License().name("Apache 2.0").url("https://springdoc.org")));
+    }
+}
