06206514f9d6319490feae5d6df5df5436e5250d
Alejo2075 <alejsant75@gmail.com>
2024-05-15 00:09:39 -0500

    added tests

--- src/test/java/org/alejo2075/employees_credit/GovEmployeesCreditApplicationTests.java
+++ src/test/java/org/alejo2075/employees_credit/GovEmployeesCreditApplicationTests.java
@@ -2,12 +2,13 @@ package org.alejo2075.employees_credit;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
-class GovEmployeesCreditApplicationTests {
+class BasicApplicationTests {
 
     @Test
     void contextLoads() {
+        assertThat(true).isTrue();
     }
-
 }
